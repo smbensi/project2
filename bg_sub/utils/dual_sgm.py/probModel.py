38c885078dd6d27e0837c617d22f16a158d7feaa
Mathias <mathiasbensimhon@gmail.com>
2023-01-21 21:46:53 +0200

    updating mean

--- bg_sub/utils/dual_sgm.py/probModel.py
+++ bg_sub/utils/dual_sgm.py/probModel.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import correlate2d
 
 class ProbModel:
     def __init__(self,frame_gray) -> None:
@@ -16,3 +17,20 @@ class ProbModel:
         self.mean_comp = np.zeros((self.NUM_MODELS,self.HEIGHT_GRIDS,self.WIDTH_GRIDS))
         self.var_comp = np.zeros((self.NUM_MODELS,self.HEIGHT_GRIDS,self.WIDTH_GRIDS))
         self.age_comp = np.ones((self.NUM_MODELS,self.HEIGHT_GRIDS,self.WIDTH_GRIDS))
+
+        # for each grid if we work with the apparent or candidate model
+        self.index_model = np.zeros((self.HEIGHT_GRIDS,self.WIDTH_GRIDS))
+
+        self.prev_frame = frame_gray
+        self.frame = frame_gray
+
+    def calc_M_t(self):
+        kernel = np.ones((self.GRID_SIZE,self.GRID_SIZE))
+        M_t = correlate2d(self.frame, kernel, mode="valid")
+        M_t = (1/self.GRID_SIZE**2)*M_t[::self.GRID_SIZE,::self.GRID_SIZE]
+        return M_t
+
+    def update_mean(self):
+        lr = self.age_comp/(self.age_comp + 1)
+        M_t = self.calc_M_t
+        self.mean = lr * self.mean_comp + (1-lr) * M_t
