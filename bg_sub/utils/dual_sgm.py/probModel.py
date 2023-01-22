2f2a86c08a9563819db24b35f4c0bc3d1c55f792
Mathias <mathiasbensimhon@gmail.com>
2023-01-22 08:59:29 +0200

    update variance and age added

--- bg_sub/utils/dual_sgm.py/probModel.py
+++ bg_sub/utils/dual_sgm.py/probModel.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.signal import correlate2d
 
-class ProbModel:
+class DualGM:
     def __init__(self,frame_gray) -> None:
         self.NUM_MODELS = 2
         self.GRID_SIZE = 4
@@ -24,13 +24,36 @@ class ProbModel:
         self.prev_frame = frame_gray
         self.frame = frame_gray
 
+    def update_params(self,frame_gray):
+        self.prev_frame = self.frame
+        self.frame = frame_gray
+
     def calc_M_t(self):
         kernel = np.ones((self.GRID_SIZE,self.GRID_SIZE))
         M_t = correlate2d(self.frame, kernel, mode="valid")
         M_t = (1/self.GRID_SIZE**2)*M_t[::self.GRID_SIZE,::self.GRID_SIZE]
         return M_t
 
+    def calc_V_t(self):
+        max_in_grids = np.zeros_like(self.mean[1])
+        diff = (self.mean[self.index_model] - self.frame)**2
+        for i in range(self.HEIGHT_GRIDS-1):
+            for j in range(self.WIDTH_GRIDS-1):
+                max_in_grids[i,j] = np.max(diff[i*self.GRID_SIZE:(i+1)*self.GRID_SIZE,\
+                                            j*self.GRID_SIZE:(j+1)*self.GRID_SIZE])
+        V_t = max_in_grids
+        return V_t
+
+
     def update_mean(self):
         lr = self.age_comp/(self.age_comp + 1)
         M_t = self.calc_M_t
         self.mean = lr * self.mean_comp + (1-lr) * M_t
+    
+    def update_var(self):
+        lr = self.age_comp/(self.age_comp + 1)
+        V_t = self.calc_V_t
+        self.var = lr * self.var_comp + (1-lr) * V_t
+    
+    def update_age(self):
+        self.age = self.age_comp + 1
